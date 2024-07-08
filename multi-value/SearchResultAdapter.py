ce9796982e7e3b994b3c1777ef140c80ab2a6526
HDJ <hayco.de.jong@di.huc.knaw.nl>
2024-07-08 16:19:12 +0200

    WIP: python version of 'republic' indexer

---
+++ multi-value/SearchResultAdapter.py
@@ -0,0 +1,44 @@
+from collections.abc import Generator
+from typing import Dict, Any
+
+from annorepo.client import ContainerAdapter
+
+from SearchResultItem import SearchResultItem
+
+
+class SearchResultAdapter:
+
+    def __init__(self, container: ContainerAdapter, query: Dict[str, Any]):
+        self.container = container
+        self.search_info = container.create_search(query)
+        self.cached_hits = -1
+
+    def hits(self) -> int:
+        if self.cached_hits == -1:
+            # check upstream cached hits first, without asking for result page
+            self.cached_hits = self.container.read_search_info(self.search_info.id)['hits']
+
+        if self.cached_hits == -1:
+            # force 'hits' to be computed by asking for first page of results
+            self.container.read_search_result_page(self.search_info.id)
+            self.cached_hits = self.container.read_search_info(self.search_info.id)['hits']
+
+        return self.cached_hits
+
+    def items(self, start_page: int = 0) -> Generator[SearchResultItem]:
+        cur_page = start_page
+        while True:
+            res = self.container.read_search_result_page(self.search_info.id, cur_page)
+            if 'items' not in res:
+                break
+
+            for item in res['items']:
+                yield SearchResultItem(item)
+
+            if 'next' not in res:
+                break
+            next_page_url = res['next']
+
+            if '?page=' not in next_page_url:
+                break
+            cur_page = next_page_url.split('?page=')[1]
---
+++ multi-value/index.py
@@ -0,0 +1,90 @@
+from typing import List, Dict
+
+from annorepo.client import AnnoRepoClient, ContainerAdapter
+from elasticsearch import Elasticsearch
+from tqdm import tqdm
+
+from SearchResultAdapter import SearchResultAdapter
+from SearchResultItem import SearchResultItem
+from SparseList import SparseList
+
+# setup annorepo
+annorepo = AnnoRepoClient('https://annorepo.republic-caf.diginfra.org')
+container = annorepo.container_adapter("republic-2024.06.18")
+print(annorepo.get_about())
+
+# setup elastic
+elastic = Elasticsearch('http://localhost:9200')
+print(elastic.info())
+
+
+def fetch_overlapping_volume_annos(container: ContainerAdapter, vol: SearchResultItem, types: List[str]) \
+        -> Dict[str, List[SearchResultItem]]:
+    query = build_overlapping_types_query(vol, types)
+
+    volume_annos = dict()
+    for t in types:
+        volume_annos[t] = list()
+
+    overlapping_anno_search = SearchResultAdapter(container, query)
+    # print(overlapping_anno_search.search_info)
+
+    anno_count = 0
+    pbar = tqdm(overlapping_anno_search.items(), total=overlapping_anno_search.hits(), leave=False, unit="ann")
+    for anno in pbar:
+        pbar.set_description(f'ann: {anno.path('body.id').removeprefix('urn:republic:'):>60}')
+        volume_annos[anno.path('body.type')].append(anno)
+        anno_count += 1
+
+    assert anno_count == overlapping_anno_search.hits()
+
+    return volume_annos
+
+
+def build_overlapping_types_query(vol: SearchResultItem, types: List[str]):
+    target = vol.first_target_with_selector('Text')
+    selector = target['selector']
+    return {"body.type": {":isIn": types},
+            ":overlapsWithTextAnchorRange": {
+                "source": target['source'],
+                "start": selector['start'],
+                "end": selector['end']
+            }}
+
+
+def index_annos(volume_annos: Dict[str, List[SearchResultItem]], main_type: str):
+    lst = SparseList()
+    aux_annos_by_main_anno = dict()
+    for main_anno in volume_annos[main_type]:
+        aux_annos = set()  # use a single anno set for each 'Resolution'
+        aux_annos_by_main_anno[main_anno] = aux_annos
+        selector = main_anno.first_target_with_selector('LogicalText')['selector']
+        for i in range(selector['start'], selector['end'] + 1):
+            lst[i] = aux_annos
+
+    for aux_type in [k for k in volume_annos.keys() if k != main_type]:
+        for aux_anno in volume_annos[aux_type]:
+            selector = aux_anno.first_target_with_selector('LogicalText')['selector']
+            already_added = None
+            for i in range(selector['start'], selector['end'] + 1):
+                if lst[i] is not None and lst[i] != already_added:
+                    lst[i].add(aux_anno)
+                    already_added = lst[i]
+
+    for main_anno in volume_annos[main_type]:
+        print(f'{main_anno.path('body.id')}: ')
+        for aux_anno in aux_annos_by_main_anno[main_anno]:
+            print(f' - {aux_anno.path('body.id')}')
+
+
+volume_search = SearchResultAdapter(container, {"body.type": "Volume"})
+volume_count = 0
+pbar = tqdm(volume_search.items(), total=volume_search.hits(), unit="vol")
+for v in pbar:
+    pbar.set_description(f'vol: {v.path('body.id').removeprefix('urn:republic:volume:')}')
+    volume_annos = fetch_overlapping_volume_annos(container, v, ['Resolution', 'Attendant', 'Entity'])
+    index_annos(volume_annos, 'Resolution')
+    volume_count += 1
+    break  # dev limit: stop after first
+
+print(f'indexed {volume_count} volumes')
